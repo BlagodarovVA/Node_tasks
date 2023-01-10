e5aea6e60c7ee7b4573e73ce105e4d06e72d7c38
Valery <11w-walerik@mail.ru>
2023-01-10 17:42:33 +0300

    39 task done

--- 1_JS_2_course/39-foreach-with-index/finish.js
+++ 1_JS_2_course/39-foreach-with-index/finish.js
@@ -9,4 +9,16 @@
  * и выведите в консоль результат вызова функции
  */
 
-const myCities = ['London', 'New York', 'Singapore']
+const myCities = ['London', 'New York', 'Singapore'];
+
+
+function cityInfo(city, index) {
+    return console.log(`${city} is at the index ${index} in the myCities array`);
+}
+
+
+cityInfo(myCities[0], 0);
+console.log('---------------------------------------');
+
+
+myCities.forEach((city, index) => cityInfo(city, index));
